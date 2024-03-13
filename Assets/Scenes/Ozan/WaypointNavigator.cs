0d5d037c08da0c0bc4450e42c7f05aa35875b24a
Ahmet Ozan Özgür <aozanozgur@hotmail.com>
2024-03-14 01:37:08 +0300

    Waypoint based AI Car Traffic
    Looped Waypoint Route, Waypoint Navigator, Waypoint Manager Window, Car Navigator

---
+++ Assets/Scenes/Ozan/WaypointNavigator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNavigator : MonoBehaviour
+{
+
+    CarNavigationController controller;
+    public Waypoint currentWaypoint;
+
+   
+    private void Awake()
+    {
+        controller = GetComponent <CarNavigationController>();
+    }
+
+    
+    void Start()
+    {
+        controller.SetDestination(currentWaypoint.GetPosition());
+    }
+
+    void Update()
+    {
+        if(controller.reachedDestination)
+        {
+            currentWaypoint = currentWaypoint.nextWaypoint;
+            controller.SetDestination(currentWaypoint.GetPosition());
+        }
+    }
+}
